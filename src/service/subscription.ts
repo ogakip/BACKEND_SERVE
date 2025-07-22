338e94fcd72be76d1b5c66592042846728f6d767
ogakip <pauloogaki.principal@gmail.com>
2025-07-22 17:04:49 -0300

    listar todos os planos disponiveis

---
+++ src/service/subscription.ts
@@ -0,0 +1,14 @@
+import { AppDataSource } from "../database/datasource";
+import { Plans } from "../entities/plans";
+import { Licenses } from "../entities/licenses";
+import { Subscriptions } from "../entities/subscriptions";
+
+const PlansRepository = AppDataSource.getRepository(Plans)
+const LicensesRepository = AppDataSource.getRepository(Licenses)
+const SubscriptionsRepository = AppDataSource.getRepository(Subscriptions)
+
+export const ListAllPlansService = async () => {
+    const getAll = await PlansRepository.find();
+
+    return getAll
+}
