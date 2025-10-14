982f06f240882a6f81755a6876d724fd28a247d6
ogakip <pauloogaki.principal@gmail.com>
2025-10-14 18:10:56 -0300

    mudancas que a ia fez que desbugou meu codigo

--- src/middlewares/findRestaurant.ts
+++ src/middlewares/findRestaurant.ts
@@ -6,6 +6,9 @@ import { messages } from '../errors/messages';
 const RestaurantRepository = AppDataSource.getRepository(Restaurant);
 
 export const checkIfRestaurantExists = async (restaurant_id: number) => {
+    if (restaurant_id === undefined || restaurant_id === null || Number.isNaN(Number(restaurant_id))) {
+        throw new AppError(messages.RESTAURANT_NOT_FOUND);
+    }
     const findRestaurant = await RestaurantRepository.findOneBy({ id: restaurant_id })
 
     if (!findRestaurant) {
